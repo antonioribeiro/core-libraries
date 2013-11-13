f9eac21b9b1b15bd89c7c22a0c2dc7f126405ae6
Antonio Carlos Ribeiro <afaria@alerj.rj.gov.br>
2013-11-13 12:03:17 -0200

    creating getAllColumnsNames() method

--- BaseModel.php
+++ BaseModel.php
@@ -129,4 +129,47 @@ class BaseModel extends \Eloquent {
 	{
 		return $this->safeColumns();
 	}
+
+	public function getAllColumnsNames()
+	{
+		switch (DB::connection()->getConfig('driver')) {
+			case 'pgsql':
+				$query = "SELECT column_name FROM information_schema.columns WHERE table_name = '".$this->table."'";
+				$column_name = 'column_name';
+				$reverse = true;
+				break;
+
+			case 'mysql':
+				$query = 'SHOW COLUMNS FROM '.$this->table;
+				$column_name = 'Field';
+				$reverse = false;
+				break;
+
+			case 'sqlsrv':
+				$query = "SELECT column_name FROM ".DB::connection()->getConfig('database').".INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'".$this->table."'";
+				$column_name = 'column_name';
+				$reverse = false;
+				break;
+
+			default: 
+				$error = 'Database driver not supported: '.DB::connection()->getConfig('driver');
+				throw new Exception($error);
+				break;
+		}
+
+		$columns = array();
+
+		foreach(DB::select($query) as $column)
+		{
+			$columns[] = $column->$column_name;
+		}
+
+		if($reverse)
+		{
+			$columns = array_reverse($columns);
+		}
+		
+		return $columns;
+	}
+
 }
